c6ea874dcba4b78cbc4a95457de08eac76f9b050
Gayatripatro <62832475+Gayatripatro@users.noreply.github.com>
2024-10-21 11:55:53 +0530

    Update Pytest-Parametrization.py

--- Pytest-Parametrization.py
+++ Pytest-Parametrization.py
@@ -1,7 +1,11 @@
 # How to declare parametrizations
-  > jjd
-  >
-  >
+  > Using parametrizations we can provide multiple test data for single input methods
+  > We can pass the test data in the method itself or using excel also we can provide the test data
+
+    > in parametrizations.py files
+    --------------------------------------------------
+    
+  
 
 
 
